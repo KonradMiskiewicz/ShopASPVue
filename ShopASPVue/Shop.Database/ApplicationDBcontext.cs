1b70aec0bda55d8cc9338dc9052d333575167a1f
KonradMiskiewicz <n4dregner@gmail.com>
2019-05-10 22:33:56 +0200

    Migrations project

---
+++ ShopASPVue/Shop.Database/ApplicationDBcontext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Shop.Domain.Models;
+
+namespace Shop.Database
+{
+    public class ApplicationDBcontext : IdentityDbContext
+    {
+        public ApplicationDBcontext(DbContextOptions<ApplicationDBcontext> options)
+            : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}

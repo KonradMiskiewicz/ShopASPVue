ef140dfea8d9f7a8a71c01d83e934d84cb8d1861
KonradMiskiewicz <n4dregner@gmail.com>
2019-05-10 22:33:22 +0200

    Configure migrations

---
+++ ShopASPVue/Shop.Application/CRUDProduct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Shop.Database;
+using Shop.Domain.Models;
+
+namespace Shop.Application
+{
+    public class CRUDProduct
+    {
+        private ApplicationDBcontext _context;
+        public CRUDProduct(ApplicationDBcontext context)
+        {
+            _context = context;
+        }
+
+        public void Do(int id, string Name, string Description)
+        {
+            _context.Products.Add(new Product { Id_Product = id, Name = Name, Description = Description });
+        }
+    }
+}

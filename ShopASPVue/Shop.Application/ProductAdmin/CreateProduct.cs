701245723e9a594f42130db2bf206ad6a1658220
KonradMiskiewicz <n4dregner@gmail.com>
2019-06-07 22:01:14 +0200

    admin crud functions

---
+++ ShopASPVue/Shop.Application/ProductAdmin/CreateProduct.cs
@@ -0,0 +1,51 @@
+﻿using Shop.Database;
+using Shop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.ProductAdmin
+{
+    public class CreateProduct
+    {
+        private ApplicationDBcontext _context;
+        public CreateProduct(ApplicationDBcontext context)
+        {
+            _context = context;
+
+        }
+        public async Task<Response> Add(Request vn)
+        {
+            var product = new Product
+            {
+                Name = vn.Name,
+                Description = vn.Description,
+                Value = vn.Value
+
+            };
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return new Response
+            {
+                Id = product.Id_Product,
+                Name = product.Name,
+                Description = product.Description,
+                Value = product.Value
+            };
+        }
+        public class Request
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public decimal Value { get; set; }
+        }
+        public class Response
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public decimal Value { get; set; }
+        }
+    }
+}
---
+++ ShopASPVue/Shop.Application/ProductAdmin/GetProduct.cs
@@ -0,0 +1,34 @@
+﻿using Shop.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shop.Application.ProductAdmin
+{
+    public class GetProduct
+    {
+        private ApplicationDBcontext _ctx;
+
+        public GetProduct(ApplicationDBcontext context)
+        {
+            _ctx = context;
+        }
+
+        public ProductViewModel Do(int id) =>
+          _ctx.Products.Where(x => x.Id_Product == id).Select(x => new ProductViewModel
+          {
+              Id = x.Id_Product,
+              Name = x.Name,
+              Description = x.Description,
+              Value = x.Value}).FirstOrDefault();
+
+        public class ProductViewModel
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public decimal Value { get; set; }
+        }
+    }
+}

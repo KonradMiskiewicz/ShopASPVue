c66641dd69aa90a9e532222ec53bd4f50cfed0a2
KonradMiskiewicz <n4dregner@gmail.com>
2019-06-19 19:09:00 +0200

    changes for order model

--- ShopASPVue/Model/Models/Order.cs
+++ ShopASPVue/Model/Models/Order.cs
@@ -8,7 +8,7 @@ namespace Shop.Domain.Models
     public class Order
     {
         public int OrderID { get; set; }
-        public string OrderRed { get; set; }
+        public string OrderRef { get; set; }
         public string StripeReference { get; set; }
         public string First_Name { get; set; }
         public string Last_Name { get; set; }
@@ -18,6 +18,6 @@ namespace Shop.Domain.Models
         public string Adress2 { get; set; }
         public string City { get; set; }
         public string PostCode { get; set; }
-        public ICollection<OrderProduct> orderProducts { get; set; }
+        public ICollection<OrderStock> orderStocks { get; set; }
     }
 }

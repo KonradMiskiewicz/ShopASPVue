24ef3433b0411639018b32e665b5f37686c4ac2a
KonradMiskiewicz <n4dregner@gmail.com>
2019-06-19 19:11:10 +0200

    method for creating orders and geting specific order

---
+++ ShopASPVue/Shop.Application/Orders/GetOrder.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shop.Application.Orders
+{
+    public class GetOrder
+    {
+        private ApplicationDBcontext _ctx;
+
+        public GetOrder(ApplicationDBcontext ctx)
+        {
+            _ctx = ctx;
+        }
+        public class Response
+        {
+            public string OrderRef { get; set; }
+            public string StripeReference { get; set; }
+            public string First_Name { get; set; }
+            public string Last_Name { get; set; }
+            public string Email { get; set; }
+            public string PhoneNumber { get; set; }
+            public string Adress1 { get; set; }
+            public string Adress2 { get; set; }
+            public string City { get; set; }
+            public string PostCode { get; set; }
+            public IEnumerable<Product> Products { get; set; }
+            public string TotalValue { get; set; }
+        }
+        public class Product
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public string Value { get; set; }
+            public int Quality { get; set; }
+            public string StockDescription { get; set; }
+        }
+        public Response Do(string reference)
+        {
+           return _ctx.Orders
+                .Where(x => x.OrderRef == reference)
+                .Include(x => x.orderStocks)
+                .ThenInclude(x => x.Stock)
+                .ThenInclude(x => x.Product)
+                .Select(x => new Response
+                {
+                    OrderRef = x.OrderRef,
+                    First_Name = x.First_Name,
+                    Last_Name = x.Last_Name,
+                    Email = x.Email,
+                    PhoneNumber = x.PhoneNumber,
+                    Adress1 = x.Adress1,
+                    Adress2 = x.Adress2,
+                    City = x.City,
+                    PostCode = x.PostCode,
+                    Products = x.orderStocks.Select(y => new Product
+                    {
+                        Name = y.Stock.Product.Name,
+                        Description = y.Stock.Product.Description,
+                        Value = $"€ {y.Stock.Product.Value.ToString("N2")}",
+                        Quality = y.Quality,
+                        StockDescription = y.Stock.Description
+                    }),
+                    TotalValue = x.orderStocks.Sum(y => y.Stock.Product.Value).ToString("N2")
+                    
+                }).FirstOrDefault();
+        }
+    }
+}

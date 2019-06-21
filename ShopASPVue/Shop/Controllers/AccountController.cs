c85bed0fd7114321e9504af40ef983433ddc92b8
KonradMiskiewicz <n4dregner@gmail.com>
2019-06-21 17:11:03 +0200

    controller for accounts

---
+++ ShopASPVue/Shop/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+/// <summary>
+/// This is account controller for call the actions
+/// </summary>
+namespace Shop.Controllers
+{
+    
+    public class AccountController : Controller
+    {
+        private SignInManager<IdentityUser> _signManager;
+
+        public AccountController(SignInManager<IdentityUser> signManager)
+        {
+            _signManager = signManager;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await _signManager.SignOutAsync();
+
+            return RedirectToPage("/Index");
+        }
+    }
+}

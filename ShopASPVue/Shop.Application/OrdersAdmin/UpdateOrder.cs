fe489001ee066b9485d4adc87965f787b90d4135
KonradMiskiewicz <n4dregner@gmail.com>
2019-06-22 15:57:04 +0200

    adding functionality of orders in admin site

---
+++ ShopASPVue/Shop.Application/OrdersAdmin/UpdateOrder.cs
@@ -0,0 +1,25 @@
+﻿using Shop.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.OrdersAdmin
+{
+    public class UpdateOrder
+    {
+        private ApplicationDBcontext _ctx;
+
+        public UpdateOrder(ApplicationDBcontext ctx)
+        {
+            _ctx = ctx;
+        }
+        public async Task<bool> Do(int id)
+        {
+            var order = _ctx.Orders.FirstOrDefault(x => x.OrderID == id);
+            order.Status = order.Status + 1;
+            return await _ctx.SaveChangesAsync() > 0;
+        }
+    }
+}

660c589e72b1ecd34180a368d9a071b19149abab
KonradMiskiewicz <n4dregner@gmail.com>
2019-06-22 15:56:15 +0200

    changes of architecture of controllers and adding order controller

---
+++ ShopASPVue/Shop/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Application.ProductAdmin;
+using Shop.Application.Products;
+using Shop.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Controllers
+{
+    [Route("[controller]")]
+    [Authorize(Policy = "Manager")]
+    public class ProductsController : Controller
+    {
+        private ApplicationDBcontext _ctx;
+
+        public ProductsController(ApplicationDBcontext ctx)
+        {
+            _ctx = ctx;
+        }
+        [HttpGet("")]
+        public IActionResult GetProducts() => Ok(new GetProducts(_ctx).Listing());
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(int id) => Ok(new Application.ProductAdmin.GetProduct(_ctx).Do(id));
+        [HttpPost("")]
+        public async Task<IActionResult> CreateProductAsync([FromBody] CreateProduct.Request vm) => Ok(await new CreateProduct(_ctx).Add(vm));
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProductAsync(int id) => Ok(await new DeleteProduct(_ctx).Delete(id));
+        [HttpPut("")]
+        public async Task<IActionResult> UpdateProductAsync([FromBody] UpdateProduct.Request vm) => Ok(await new UpdateProduct(_ctx).Update(vm));
+    }
+}
---
+++ ShopASPVue/Shop/Controllers/StocksController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Application.StockAdmin;
+using Shop.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Controllers
+{
+    [Route("[controller]")]
+    [Authorize(Policy = "Manager")]
+    public class StocksController : Controller
+    {
+        private ApplicationDBcontext _ctx;
+
+        public StocksController(ApplicationDBcontext ctx)
+        {
+            _ctx = ctx;
+        }
+        [HttpGet("")]
+        public IActionResult GetStocks() => Ok(new GetStock(_ctx).ListingStock());
+        [HttpPost("")]
+        public async Task<IActionResult> CreateStockAsync([FromBody] CreateStock.Request vm) => Ok(await new CreateStock(_ctx).Add(vm));
+        [HttpDelete("/{id}")]
+        public async Task<IActionResult> DeleteStockAsync(int id) => Ok(await new DeleteStock(_ctx).Delete(id));
+        [HttpPut("")]
+        public async Task<IActionResult> UpdateStockAsync([FromBody] UpdateStock.Request vm) => Ok(await new UpdateStock(_ctx).Update(vm));
+    }
+}

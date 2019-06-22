4bdde95724b9e9e2b87887a5d2c78047acf6c80a
KonradMiskiewicz <n4dregner@gmail.com>
2019-06-22 15:59:15 +0200

    adding view for adding new users

---
+++ ShopASPVue/Shop.Application/UserAdmin/CreateUser.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.UserAdmin
+{
+    public class CreateUser
+    {
+        private UserManager<IdentityUser> _user;
+
+        public CreateUser(UserManager<IdentityUser> user)
+        {
+            _user = user;
+        }
+        public class Request
+        {
+            public string UserName { get; set; }
+        }
+        public async Task<bool> Do(Request request)
+        {
+            var managerUser = new IdentityUser
+            {
+                UserName = request.UserName
+            };
+            await _user.CreateAsync(managerUser, "password");
+            var managerClaim = new Claim("Role", "Manager");
+            await _user.AddClaimAsync(managerUser, managerClaim);
+            return true;
+        }
+    }
+}

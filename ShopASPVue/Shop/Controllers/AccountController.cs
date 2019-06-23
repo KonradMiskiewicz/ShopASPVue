c597e81cf662873b3dcf8e59ad7579e1c4cfffdc
KonradMiskiewicz <n4dregner@gmail.com>
2019-06-23 16:59:02 +0200

    adding comments to code

--- ShopASPVue/Shop/Controllers/AccountController.cs
+++ ShopASPVue/Shop/Controllers/AccountController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 /// <summary>
-/// This is account controller for call the actions
+/// This is account controller for Logout function
 /// </summary>
 namespace Shop.Controllers
 {
@@ -18,6 +18,12 @@ namespace Shop.Controllers
         {
             _signManager = signManager;
         }
+        /// <summary>
+        /// Method from SignInManager called Signoutasync for Logout Admin 
+        /// </summary>
+        /// <returns>
+        /// Index Page
+        /// </returns>
         [HttpGet]
         public async Task<IActionResult> Logout()
         {
--- ShopASPVue/Shop/Controllers/ProductsController.cs
+++ ShopASPVue/Shop/Controllers/ProductsController.cs
@@ -7,7 +7,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+/// <summary>
+/// Products controller for communication view and methods from application project
+/// </summary>
 namespace Shop.Controllers
 {
     [Route("[controller]")]
@@ -20,14 +22,46 @@ namespace Shop.Controllers
         {
             _ctx = ctx;
         }
+        /// <summary>
+        /// Get all products and convert to List
+        /// </summary>
+        /// <returns>
+        /// List Of products
+        /// </returns>
         [HttpGet("")]
         public IActionResult GetProducts() => Ok(new GetProducts(_ctx).Listing());
+        /// <summary>
+        /// Get specific product from id parameter
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>
+        /// Specific Product
+        /// </returns>
         [HttpGet("{id}")]
         public IActionResult GetProduct(int id) => Ok(new Application.ProductAdmin.GetProduct(_ctx).Do(id));
+        /// <summary>
+        /// Create product method async 
+        /// </summary>
+        /// <param name="vm"></param>
+        /// <returns>
+        /// DBcontext
+        /// </returns>
         [HttpPost("")]
         public async Task<IActionResult> CreateProductAsync([FromBody] CreateProduct.Request vm) => Ok(await new CreateProduct(_ctx).Add(vm));
+        /// <summary>
+        /// Delete Product taken from id param async
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteProductAsync(int id) => Ok(await new DeleteProduct(_ctx).Delete(id));
+        /// <summary>
+        /// Update product async
+        /// </summary>
+        /// <param name="vm"></param>
+        /// <returns>
+        /// DBcontext
+        /// </returns>
         [HttpPut("")]
         public async Task<IActionResult> UpdateProductAsync([FromBody] UpdateProduct.Request vm) => Ok(await new UpdateProduct(_ctx).Update(vm));
     }
